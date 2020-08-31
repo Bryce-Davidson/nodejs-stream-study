7ec3750aa23faff371e980f65e0a2c6e2cecedf9
Bryce <bryce_davidson@icloud.com>
2020-08-31 14:39:51 -0700

    Working added references

--- stream-transfer.js
+++ stream-transfer.js
@@ -3,10 +3,17 @@ const fs = require('fs');
 const src = fs.createReadStream('./big.txt'); 
 const end = fs.createWriteStream('./write.txt')
 
+// Even though this changes the path, it will still
+// Only write to the ./write.txt
+console.log(src.path);
+src.path = './new.txt'
+console.log(src.path);
 
+
+// Hook into stream events
 src.on('data', (chunk) => {
     console.log(chunk.toString())
-    setTimeout(()=> {}, 1000)
+    src.pause();
 })
 
 src.pipe(end)
